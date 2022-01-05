9f1325959d09940a4a647c87bd6887e73fa3b0b4
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-05 15:48:37 -0500

    Implement StartGame logic to run through all of the steps of the game

--- StartGame.rb
+++ StartGame.rb
@@ -7,3 +7,21 @@ class StartGame
     end
   
   end
+
+  def play_game
+    puts "Put in a number"
+    while !game.game_is_over
+      game.get_question_and_answer
+      puts game.question
+      user_input = gets.chomp.to_i
+      game.check_response(user_input)
+      puts game.validation_response 
+      puts "#{game.current_player.user_id}: #{game.current_player.lives}/3 #{game.current_opponent.user_id}: #{game.current_opponent.lives}/3 "
+      if !game.game_is_over
+        puts "-~*----NEW TURN----*~-"
+      end
+      game.switch_players
+    end
+    puts "-~*----GAME OVER----*~-"
+    puts "#{game.current_player.user_id} wins with a score of #{game.current_player.lives}/3 "
+  end
--- main.rb
+++ main.rb
@@ -6,14 +6,5 @@ require './StartGame'
 player_one = Player.new("Frida", 3)
 player_two = Player.new("John", 3)
 game = Game.new(player_one, player_two)
-StartGame.new(game)
-
-# game.get_question_and_answer
-# game.check_response(1)
-# puts game.validation_response
-# puts game.current_player.lives
-
-# game.switch_players
-# game.check_response(10)
-# puts game.validation_response
-# puts game.current_opponent.lives
+start = StartGame.new(game)
+start.play_game
