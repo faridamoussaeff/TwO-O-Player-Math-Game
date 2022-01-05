48243995eac38f7d0d92e27855d9fc11fa821641
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-05 15:20:24 -0500

    Implement switch_players and get_question_and_answer_logic in Game class

--- Game.rb
+++ Game.rb
@@ -1,3 +1,26 @@
+require "./Question"
+
 class Game
+    @@question_instance = Question.new
+
+    attr_accessor :current_player, :current_opponent, :answer, :question
+  
+    def initialize(current_player, current_opponent)
+      @current_player = current_player
+      @current_opponent = current_opponent
+    end 
+  
+    def switch_players 
+      p1 = @current_player
+      p2 = @current_opponent
+      @current_player = p2
+      @current_opponent = p1
+    end
+  
+    def get_question_and_answer
+      @@question_instance.generate_inputs
+      @question = "Question for #{@current_player.user_id}: What is the sum of #{@@question_instance.input_one} and #{@@question_instance.input_two}?"
+      @answer =  @@question_instance.calculate_answer
+    end
 
 end 
--- main.rb
+++ main.rb
@@ -3,25 +3,23 @@ require './Question'
 require './Game'
 
 player_one = Player.new("Frida", 3)
-# puts player_one.user_id
-# player_copy = player_one
-# player_copy.user_id = "hM"
-# puts player_one.user_id
-# puts player_one.lives
-# player_one.remove_life
-# puts player_one.lives
+player_two = Player.new("John", 3)
+game = Game.new(player_one, player_two)
+puts game.current_player.user_id
+puts game.current_opponent.user_id
+game.current_player.remove_life
+puts game.current_player.lives
 
-# player_copy.remove_life
-# puts player_one.lives
+game.switch_players
+puts game.current_player.user_id
+puts game.current_opponent.user_id
+puts game.current_opponent.lives
+puts game.current_player.lives
 
-question_one = Question.new
-puts question_one.input_one
-question_one.generate_inputs
-puts question_one.input_one
-puts question_one.input_two
-puts question_one.calculate_answer
+game.get_question_and_answer
+puts game.answer 
+puts game.question 
 
-question_one.generate_inputs
-puts question_one.input_one
-puts question_one.input_two
-puts question_one.calculate_answer 
+game.get_question_and_answer
+puts game.answer 
+puts game.question 
